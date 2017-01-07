9db87af426ba82dabc007d6219f6118ba67725c3
Trevor Renshaw <trevren11@gmail.com>
2017-01-06 17:43:41 -0700

    Add 90 minute automatic logout

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,55 +1,62 @@
 // Enables navigation with the administrative sidebar
+var oneMinute = 1000 * 60;
 (function leftNavBar() {
-  window.addEventListener('popstate', function(evt) {
-    if(!evt.state) { return; }
+  window.addEventListener('popstate', function (evt) {
+    if (!evt.state) { return; }
     loadPage(evt.state.title, evt.state.href);
   });
 
   function loadPage(title, href) {
-    if(!href || href === '/') {
+    if (!href || href === '/') {
       $("#loader").html('');
       return;
     }
     document.getElementById("caption").classList.remove("aboveLoader");
-    if(href.indexOf('?') === - 1) {
+    if (href.indexOf('?') === - 1) {
       href += '?ajax=true';
     }
-    else
-    {
+    else {
       href += '&ajax=true';
     }
     checkForUnread();
 
     document.title = title + ' | HLRDesk';
-    $( "#loader" ).load( href );
+    $("#loader").load(href);
   }
 
-  $(".lpanel[data-title]").click(function loadMsg(ev){
+  $(".lpanel[data-title]").click(function loadMsg(ev) {
     var href = $(this).attr('href');
     var title = $(this).data('title');
 
-    history.pushState({href: href, title: title}, title, href);
+    history.pushState({ href: href, title: title }, title, href);
     ev.preventDefault();
     loadPage(title, href);
   });
   // TODO: account for query strings
   var href = location.pathname + location.search;
   var title = document.title;
-  history.pushState({href: href, title: title}, title, href);
+  history.pushState({ href: href, title: title }, title, href);
 
-  $('document').ready(function(){checkForUnread();});
+  $('document').ready(function () {
+    checkForUnread();
+    // check if the timer is past
+    if (localStorage.timer == undefined) {
+      localStorage.timer = Date.now() + oneMinute * 90; // hour and a half
+    }
+  });
+
   var socket = window.io();
-  function checkForUnread(){
-    socket.emit('unread message', {token: window.HLRDESK.token});
+  function checkForUnread() {
+    socket.emit('unread message', { token: window.HLRDESK.token });
   };
 
-  socket.on('unread message', function(){
+  socket.on('unread message', function () {
     $("#messageDisplay").show().one();
   });
 
-  socket.on('expired token', function(service){
+  socket.on('expired token', function (service) {
     window.HLRDESK.alert.error('Your session has expired. Please log in again.');
-    window.setTimeout(function(){
+    window.setTimeout(function () {
       window.location.href = '/logout';
     }, 3000);
   });
@@ -57,6 +64,16 @@
 
 window.HLRDESK.init.messages();//not sure this is the best way to load messages sockets
 
-function showMessage(){
+function showMessage() {
   $("#messageDisplay").show().one();
+}
+
+function checkTimeout() {
+  if (localStorage.timer == undefined || localStorage.timer < Date.now()) {
+    localStorage.removeItem("timer");
+    window.location.href = '/logout';
   }
+  setTimeout(checkTimeout, oneMinute / 2); // Check every 30 seconds for logout
+}
+// checkTimeout();
+setTimeout(checkTimeout, 5000);
